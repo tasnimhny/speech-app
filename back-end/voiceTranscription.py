ca195f0aa5cdd2a0c84ccb3de5de9b8bac8304d6
ShabirZ <shabirzahir1202@gmail.com>
2025-03-07 14:10:08 -0500

    added whisper model and audio sampling in branch, need to format

---
+++ back-end/voiceTranscription.py
@@ -0,0 +1,77 @@
+#from faster_whisper import WhisperModel
+
+"""PyAudio Example: Play a wave file."""
+import wave
+import sys
+import pyaudio
+
+
+
+
+
+"""
+model_size = WhisperModel("medium")
+
+# Run on GPU with FP16
+#model = WhisperModel(model_size, device="cuda", compute_type="float16")
+
+# or run on GPU with INT8
+# model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
+# or run on CPU with INT8
+model = WhisperModel(model_size, device="cpu", compute_type="int8")
+
+def get_text(model, audio_path):
+    segments, info = model.transcribe(audio_path, beam_size=5)
+    print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+    for segment in segments:
+        print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+
+"""
+
+
+for _ in range(5):
+
+    # the file name output you want to record into
+    filename = "voice_audio.wav"
+    # set the chunk size of 1024 samples
+    chunk = 1024
+    # sample format
+    FORMAT = pyaudio.paInt16
+    # mono, change to 2 if you want stereo
+    channels = 1
+    # 44100 samples per second
+    sample_rate = 44100
+    record_seconds = 5
+    # initialize PyAudio object
+    p = pyaudio.PyAudio()
+    # open stream object as input & output
+    stream = p.open(format=FORMAT,
+                    channels=channels,
+                    rate=sample_rate,
+                    input=True,
+                    output=True,
+                    frames_per_buffer=chunk)
+    frames = []
+    print("Recording...")
+    for i in range(int(44100 / chunk * record_seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+    print("Finished recording.")
+    # stop and close stream
+    stream.stop_stream()
+    stream.close()
+    # terminate pyaudio object
+    p.terminate()
+    # save audio file
+    # open the file in 'write bytes' mode
+    wf = wave.open(filename, "wb")
+    # set the channels
+    wf.setnchannels(channels)
+    # set the sample format
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    # set the sample rate
+    wf.setframerate(sample_rate)
+    # write the frames as bytes
+    wf.writeframes(b"".join(frames))
+    # close the file
+    wf.close()
